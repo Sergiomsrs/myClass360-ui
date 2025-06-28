22868155a7d3427089b89fcec4999d42039134a8
Sergio <sergiomsrs@gmail.com>
2025-06-28 13:01:01 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Header } from "./section/header"
+
+
+function App() {
+
+  return (
+    <>
+       <Header />
+      <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+       <div className="border-2 border-black h-96 w-2/3 flex items-center justify-center bg-white shadow-lg rounded-lg">
+       <h1 className="font-bold text-6xl">My Class 360</h1>
+
+       </div>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
